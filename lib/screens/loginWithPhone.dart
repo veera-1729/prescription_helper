adb5c74fef8c140413b52f6d7ff3486112459166
veera-1729 <veerabhadrudu1729@gmail.com>
2023-03-01 22:42:38 +0530

    commiting my changes

--- lib/screens/loginWithPhone.dart
+++ lib/screens/loginWithPhone.dart
@@ -63,6 +63,7 @@ class _LoginWithPhoneState extends State<LoginWithPhone> {
                     SizedBox(
                       height: appDimens.paddingw20,
                     ),
+                    nameContainer(),
                     emailMobileView(),
                     SizedBox(
                       height: appDimens.paddingw20,
@@ -79,10 +80,15 @@ class _LoginWithPhoneState extends State<LoginWithPhone> {
                             });
                           },
                         ),
-                        Text("Login as Admin",style: TextStyle(fontWeight: FontWeight.w900),)
+                        Text(
+                          "Login as Admin",
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        )
                       ],
                     ),
-                    SizedBox(height: 20,),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Utility.loginButtonsWidget(
                       "",
                       "Continue",
@@ -176,6 +182,68 @@ class _LoginWithPhoneState extends State<LoginWithPhone> {
               ),
               textInputAction: TextInputAction.done,
               keyboardType: TextInputType.phone,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget nameContainer() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: appDimens.paddingw16 * 2,
+        right: appDimens.paddingw16 * 2,
+        bottom: appDimens.paddingw16,
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.whiteColor,
+        // boxShadow: [
+        //   BoxShadow(
+        //     offset: Offset(0, 1),
+        //     blurRadius: 2,
+        //     color: Colors.black54,
+        //   ),
+        // ],
+        border: Border.all(color: AppColors.blackColor, width: 0.5),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Row(
+        children: <Widget>[
+          // Container(
+          //   padding: EdgeInsets.only(
+          //     left: appDimens.paddingw6,
+          //     right: appDimens.paddingw6,
+          //   ),
+          //   child: Text(
+          //     "+91",
+          //     style: TextStyle(
+          //         fontSize: appDimens.text16, color: AppColors.greyText),
+          //   ),
+          // ),
+          Container(
+            color: AppColors.blackColor,
+            width: 0.5,
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              right: appDimens.paddingw6,
+            ),
+          ),
+          Expanded(
+            child: TextFormField(
+              style: TextStyle(
+                  fontSize: appDimens.text16, color: AppColors.greyText),
+              controller: textEditingController,
+              decoration: InputDecoration(
+                hintText: "Username",
+                counterText: "",
+                hintStyle: TextStyle(color: AppColors.greyText),
+                border: InputBorder.none,
+              ),
+              textInputAction: TextInputAction.done,
+              keyboardType: TextInputType.name,
             ),
           )
         ],
