c00dc67f8049f2960bb1ff95cf25a16ce804565a
Venkata sai <venkatsaic39@gmail.com>
2023-03-09 16:21:19 +0530

    sign in

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:prescription_helper/screens/otpscreen.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -10,80 +13,116 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromRGBO(217, 225, 232, 1),
       body: Padding(
-        padding: EdgeInsets.only(top: 100.0, left: 20.0, right: 20.0),
+        padding: EdgeInsets.only(top: 100.h, left: 20.w, right: 20.w),
         child: Column(
           children: [
             Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/logo.png",
+                      width: 33.w,
+                      height: 32.h,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
                 Center(
                   child: Text(
                     'Sign In',
                     style: TextStyle(
-                      fontSize: 30.0,
+                      fontSize: 30.sp,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                SizedBox(height: 50.0),
+                SizedBox(height: 50.h),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       'Name',
                       style: TextStyle(
+                        color: Colors.grey[700],
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 10.0),
-                    TextField(
-                      
-                      decoration: InputDecoration(
-                         contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-                        hintText: "Venkat",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+                    SizedBox(height: 10.h),
+                    Container(
+                      height: 44.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.r),
+                        color: Color.fromRGBO(239, 242, 241, 1),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20.w, vertical: 5.h),
+                          hintText: "Venkat",
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.r),
+                          ),
                         ),
                       ),
                     ),
                   ],
                 ),
-                SizedBox(height: 20.0),
+                SizedBox(height: 20.h),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       'Phone Number',
                       style: TextStyle(
+                        color: Colors.grey[700],
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 10.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        contentPadding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        prefixIcon: Icon(Icons.phone),
+                    SizedBox(height: 10.h),
+                    Container(
+                      height: 44.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.r),
+                        color: Color.fromRGBO(239, 242, 241, 1),
                       ),
-                      keyboardType: TextInputType.phone,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 30.w, vertical: 10.h),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.r),
+                          ),
+                          prefixIcon: Icon(Icons.phone),
+                        ),
+                        keyboardType: TextInputType.phone,
+                      ),
                     ),
                   ],
                 ),
-                SizedBox(height: 60.0),
+                SizedBox(height: 90.h),
               ],
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: Text('Send OTP'),
+              onPressed: () {
+                Get.to(
+                  ()=>OtpScreen(),
+                );
+              },
+              child: Text(
+                'Send OTP',
+                style: TextStyle(fontSize: 18.sp),
+              ),
               style: ElevatedButton.styleFrom(
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
+                  borderRadius: BorderRadius.circular(20.r),
                 ),
-                minimumSize: Size(150, 40),
+                minimumSize: Size(180.w, 45.h),
               ),
             ),
           ],
